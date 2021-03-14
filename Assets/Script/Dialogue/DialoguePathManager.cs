6379e704b62696d651fd48bc1b02390cb8be868c
Crocobob911 <mjdaniel911@naver.com>
2021-03-14 16:00:22 +0900

    모바일 콘솔 창 추가
    2021.03.14

--- Assets/Script/Dialogue/DialoguePathManager.cs
+++ Assets/Script/Dialogue/DialoguePathManager.cs
@@ -6,16 +6,31 @@ using System.IO;
 public class DialoguePathManager : MonoBehaviour
 {
     public List<string> DialaoguePath = new List<string>();
+    string assetsPath;
 
     private void Start()
     {
         DialaoguePath.Clear();
+        PathFinder();
+        Debug.Log(assetsPath);
         PathLoad();
     }
 
     private void PathLoad()
     {
-        DialaoguePath.Add(Path.Combine(Application.dataPath, "Json/dummyTextData1.json"));
-        DialaoguePath.Add(Path.Combine(Application.dataPath, "Json/dummyTextData2.json"));
+        DialaoguePath.Add(Path.Combine(assetsPath, "Json/dummyTextData1.json"));
+        DialaoguePath.Add(Path.Combine(assetsPath, "Json/dummyTextData2.json"));
+    }
+
+    private void PathFinder()
+    {
+        if (Application.platform == RuntimePlatform.Android)
+        {
+            assetsPath = Application.streamingAssetsPath;
+        }
+        else
+        {
+            assetsPath = Application.dataPath;
+        }
     }
 }
